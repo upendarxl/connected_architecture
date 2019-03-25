36efc0530da45519e97c49f25787eabe0a39998d
upendarxl <44067873+upendarxl@users.noreply.github.com>
2019-03-25 14:09:16 +0530

    ghjn

---
+++ connected/WebForm1.aspx
@@ -0,0 +1,57 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="connected.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:Label ID="Label1" runat="server" Text="Address_ID"></asp:Label>
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="First_name"></asp:Label>
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="Label3" runat="server" Text="Last_name"></asp:Label>
+        <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="Label4" runat="server" Text="Email"></asp:Label>
+        <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="Label5" runat="server" Text="Phone"></asp:Label>
+        <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>
+        <p>
+            <asp:TextBox ID="message" runat="server" BorderColor="Red" BorderStyle="Dotted" Width="141px"></asp:TextBox>
+        </p>
+        <asp:GridView ID="GridView1" runat="server" BackColor="#DEBA84" BorderColor="#DEBA84" BorderStyle="None" BorderWidth="1px" CellPadding="3" CellSpacing="2">
+            <FooterStyle BackColor="#F7DFB5" ForeColor="#8C4510" />
+            <HeaderStyle BackColor="#A55129" Font-Bold="True" ForeColor="White" />
+            <PagerStyle ForeColor="#8C4510" HorizontalAlign="Center" />
+            <RowStyle BackColor="#FFF7E7" ForeColor="#8C4510" />
+            <SelectedRowStyle BackColor="#738A9C" Font-Bold="True" ForeColor="White" />
+            <SortedAscendingCellStyle BackColor="#FFF1D4" />
+            <SortedAscendingHeaderStyle BackColor="#B95C30" />
+            <SortedDescendingCellStyle BackColor="#F1E5CE" />
+            <SortedDescendingHeaderStyle BackColor="#93451F" />
+        </asp:GridView>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="insert" />
+            <asp:Button ID="Button2" runat="server" OnClick="update" Text="update" />
+            <asp:Button ID="Button3" runat="server" OnClick="delete" Text="Delete" />
+            <asp:Button ID="Button4" runat="server" OnClick="search" Text="Search" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ connected/WebForm1.aspx.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace connected
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+       public  SqlConnection Getconnection()
+        {
+            SqlConnection con = new SqlConnection("data source = DESKTOP-GQADM33; initial catalog = adp.net; integrated security = true");
+            return con;
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection con = Getconnection();
+            SqlCommand cmd = new SqlCommand("Inserttest", con);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@First_name", TextBox2.Text);
+            cmd.Parameters.AddWithValue("@Last_name", TextBox3.Text);
+            cmd.Parameters.AddWithValue("@Email", TextBox4.Text);
+            cmd.Parameters.AddWithValue("@Phone", TextBox5.Text);
+            con.Open();
+            int flag = cmd.ExecuteNonQuery();
+            if (flag > 0)
+            {
+                message.Text = "record inserted";
+            }
+
+            con.Close();
+
+
+        }
+
+        protected void delete(object sender, EventArgs e)
+        {
+            SqlConnection con = Getconnection();
+            SqlCommand cmd = new SqlCommand("Deletetest", con);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Addess_id", Convert.ToInt16(TextBox1.Text));
+            con.Open();
+            int flag = cmd.ExecuteNonQuery();
+            if (flag > 0)
+            {
+                message.Text = "Record deleted succesfully";
+
+            }
+            con.Close();
+
+
+            
+
+
+        }
+
+        protected void update(object sender, EventArgs e)
+        {
+            SqlConnection con = Getconnection();
+            SqlCommand cmd = new SqlCommand("Updatetest", con);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@First_name", TextBox2.Text);
+            cmd.Parameters.AddWithValue("@Address_ID",Convert.ToInt16( TextBox1.Text));
+            con.Open();
+            int flag = cmd.ExecuteNonQuery();
+            if (flag > 0)
+            {
+                message.Text = "record updates successfully";
+            }
+
+            
+
+        }
+
+        protected void search(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                using (SqlConnection con = Getconnection())
+                {
+                    SqlDataAdapter da = new SqlDataAdapter("Select * from adotest", con);
+
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    GridView1.DataSource = dt;
+                    GridView1.DataBind();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+
+        }
+    }
+}
